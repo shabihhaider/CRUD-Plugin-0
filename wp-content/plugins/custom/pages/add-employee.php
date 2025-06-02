fc6060bcce3dc4dbf0d156f71969b79fb07e5a79
shabihhaider <2022cs723@student.uet.edu.pk>
2025-06-02 03:58:34 -0700

    Layout Settings (Add, List)

--- wp-content/plugins/custom/pages/add-employee.php
+++ wp-content/plugins/custom/pages/add-employee.php
@@ -5,33 +5,56 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
  
 <div class="container">
-  <h2>Add Employee</h2>
-  <div class="panel panel-primary">
-    <div class="panel-heading">Add Employee</div>
-    <div class="panel-body">
-        <form action="/action_page.php">
-            <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-            </div>
-            <div class="form-group">
-            <label for="pwd">Password:</label>
-            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-            </div>
-            <div class="checkbox">
-            <label><input type="checkbox" name="remember"> Remember me</label>
-            </div>
-            <button type="submit" class="btn btn-success">Submit</button>
-        </form>
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>Add Employee</h2>
+      <div class="panel panel-primary">
+        <div class="panel-heading">Add Employee</div>
+        <div class="panel-body">
+            <form action="/action_page.php">
+                <div class="form-group">
+                  <label for="name">Name:</label>
+                  <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name">
+                </div>
+                <div class="form-group">
+                  <label for="email">Email:</label>
+                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <div class="form-group">
+                  <label for="pwd">Password:</label>
+                  <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+                </div>
+                <div class="form-group">
+                  <label for="phoneNo">Phone Number:</label>
+                  <input type="number" class="form-control" id="phoneNo" placeholder="Enter phone number" name="phoneNo">
+                </div>
+                <div class="form-group">
+                  <label for="gender">Gender:</label>
+                  <select name="gender" id="gender" class="form-control">
+                    <option value="">Select gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="designation">Designation:</label>
+                  <input type="text" class="form-control" id="designation" placeholder="Enter Designation" name="designation">
+                </div>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+        </div>
+      </div>
     </div>
   </div>
 </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 </body>
 </html>
--- wp-content/plugins/custom/pages/list-employee.php
+++ wp-content/plugins/custom/pages/list-employee.php
@@ -5,39 +5,57 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.3.1/css/dataTables.dataTables.min.css">
 </head>
 <body>
     <div class="container">
-        <h2>List Employee</h2>
-        <table class="table table-condensed">
-            <thead>
-            <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Email</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>John</td>
-                <td>Doe</td>
-                <td>john@example.com</td>
-            </tr>
-            <tr>
-                <td>Mary</td>
-                <td>Moe</td>
-                <td>mary@example.com</td>
-            </tr>
-            <tr>
-                <td>July</td>
-                <td>Dooley</td>
-                <td>july@example.com</td>
-            </tr>
-            </tbody>
-        </table>
+        <div class="row">
+            <div class="col-sm-10">
+                <h2>List Employee</h2>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">List Employee</div>
+                    <div class="panel-body">
+                        <table class="table table-condensed" id="tbl-employee">
+                            <thead>
+                                <tr>
+                                    <th>#ID</th>
+                                    <th>#Name</th>
+                                    <th>#Email</th>
+                                    <th>#Phone No</th>
+                                    <th>#Designation</th>
+                                    <th>#Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Shabih Haider</td>
+                                    <td>shabih@gmail.com</td>
+                                    <td>0124214241</td>
+                                    <td>Web Developer</td>
+                                    <td>
+                                        <a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+                                        <a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+                                        <a href="javascript:void(0)" class="btn btn-info">View</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
+    <script src="https://cdn.datatables.net/2.3.1/js/dataTables.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <script>
+        $(function(){
+            // List Employee Table
+            new DataTable('#tbl-employee');
+        });
+    </script>
 </body>
 </html>
