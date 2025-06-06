4709fbeb96932e659fbbf98aa0a034eb945c98dc
shabihhaider <2022cs723@student.uet.edu.pk>
2025-06-06 10:36:48 -0700

    Delete Employee Data Function

--- wp-content/plugins/custom/pages/list-employee.php
+++ wp-content/plugins/custom/pages/list-employee.php
@@ -1,6 +1,17 @@
 <?php
     global $wpdb;
-
+    $message = "";
+    
+    // Delete Employee
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (isset($_POST['empId']) && !empty($_POST['empId'])) {
+            $wpdb->delete("{$wpdb->prefix}ems_form_data", array(
+                "id" => intval($_POST['empId'])
+            ));
+            $message = "Employee Deleted Successfully";
+        }
+    }
+    
     $employees = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ems_form_data", ARRAY_A);
 ?>
 
@@ -11,6 +22,15 @@
                 <div class="panel panel-primary">
                     <div class="panel-heading">List Employee</div>
                     <div class="panel-body">
+                        <?php
+                            if (isset($message) && !empty($message)) {
+                                ?>
+                                    <div class="alert alert-success">
+                                        <?php echo $message; ?>
+                                    </div>
+                                <?php
+                            }
+                        ?>
                         <table class="table table-condensed" id="tbl-employee">
                             <thead>
                                 <tr>
@@ -37,7 +57,13 @@
                                                     <td><?php echo $employee['designation']; ?></td>
                                                     <td>
                                                         <a href="admin.php?page=ems-plugin&action=edit&empId=<?php echo $employee['id']; ?>" class="btn btn-warning">Edit</a>
-                                                        <a href="admin.php?page=ems-list-employee&action=delete&empId=<?php echo $employee['id']; ?>" class="btn btn-danger">Delete</a>
+
+                                                        <form action="<?php $_SERVER['PHP_SELF'] ?>?page=ems-list-employee" method="post" id="form-delete-employee-<?php echo $employee['id']; ?>">
+                                                            <input type="hidden" name="empId" value="<?php echo $employee['id']; ?>">
+                                                        </form>
+
+                                                        <a href="javascript:void(0)" onclick="if(confirm('Are you sure you want to delete it?')) { jQuery('#form-delete-employee-<?php echo $employee['id']; ?>').submit(); }" class="btn btn-danger">Delete</a>
+                                                        
                                                         <a href="admin.php?page=ems-plugin&action=view&empId=<?php echo $employee['id']; ?>" class="btn btn-info">View</a>
                                                     </td>
                                                 </tr>
