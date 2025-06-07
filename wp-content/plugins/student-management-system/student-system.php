5c95e9eb03e79ad139c0d05378d911459f9c4579
shabihhaider <2022cs723@student.uet.edu.pk>
2025-06-06 23:08:46 -0700

    2nd Plugin Setup by PHP Object Oriented

---
+++ wp-content/plugins/student-management-system/student-system.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+Plugin Name: Student Management System
+Plugin URI: http://example.com/plugins/student-plugin/
+Description: This is a test Student Management System Plugin
+Author: Muhammad Shabih Haider
+Version: 1.0
+Author URI: http://myPortfolio.com
+Requires at least: 6.8.1
+Requires PHP: 7.4
+*/
+
+define("SMS_PLUGIN_PATH", plugin_dir_path(__FILE__));
+define("SMS_PLUGIN_URL", plugin_dir_url(__FILE__));
+
+include_once(SMS_PLUGIN_PATH."class/StudentManagement.php"); // Add class path
+
+$studentManagementObject = new StudentManagement();
